60bb8291e08a189f1a013859909d0a21cadccddc
ICOM STUDENTS <os_100_25@s121ws09.ece.uprm.edu>
2025-01-29 19:31:22 -0400

    Prograss in person

---
+++ OSLabs1/firstProgram/build/CMakeFiles/OSLabs.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OSLabs.dir/src/functions.c.o"
+  "CMakeFiles/OSLabs.dir/src/functions.c.o.d"
+  "CMakeFiles/OSLabs.dir/src/main.c.o"
+  "CMakeFiles/OSLabs.dir/src/main.c.o.d"
+  "OSLabs"
+  "OSLabs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OSLabs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
